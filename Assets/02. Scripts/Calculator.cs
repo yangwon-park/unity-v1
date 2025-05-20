ae3253dfa8c01385c26158ca7c6fa811a6908848
yangwon-park <ywonp94@gmail.com>
2025-05-20 16:20:47 +0900

    :recycle: SerializeField 추가

--- Assets/02. Scripts/Calculator.cs
+++ Assets/02. Scripts/Calculator.cs
@@ -1,37 +1,48 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace _02._Scripts
 {
     public class Calculator : MonoBehaviour
     {
-        public int number1;
-        public int number2;
+        [SerializeField] private int number1;
+        [SerializeField] private int number2;
+
+        public int Number1
+        {
+            get => number1;
+            set => number1 = value;
+        }
+        
+        public int Number2
+        {
+            get => number2;
+            set => number2 = value;
+        }
         
         int Add()
         {
             return number1 + number2;
         }
-        
+
         int Subtract()
         {
             return number1 - number2;
         }
-        
+
         void Multiply()
         {
-            
         }
-        
+
         void Divide()
         {
-            
         }
-        
+
         void Start()
         {
             var addResult = Add();
             var substractResult = Subtract();
-            
+
             Debug.Log($"Add Result: {addResult}");
             Debug.Log($"Substract Result: {substractResult}");
         }
