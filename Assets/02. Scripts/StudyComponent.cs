7f45826e07e9ab86c318201acee61c80fdcd2654
yangwon-park <ywonp94@gmail.com>
2025-05-22 10:51:39 +0900

    :necktie: GetComponent 활용한 Component 정보 가져오기 => MeshRender enabled

--- Assets/02. Scripts/StudyComponent.cs
+++ Assets/02. Scripts/StudyComponent.cs
@@ -6,22 +6,12 @@ namespace _02._Scripts
     public class StudyComponent : MonoBehaviour
     {
         private GameObject _obj; // Unity Editor > Inspector에서 Cube 주입받음
-        [SerializeField] private string changeName;
         
         private void Start()
         {
             // _obj = GameObject.Find("Main Camera"); // Hierarchy 전체 기준으로 검색
             _obj = GameObject.FindGameObjectWithTag("Player");
-            _obj.name = changeName;
-            
-            Debug.Log(_obj.name);
-            Debug.Log(_obj.tag);
-            Debug.Log(_obj.transform.position);
-            Debug.Log(_obj.transform.rotation);
-            Debug.Log(_obj.transform.localScale);
-
-            Debug.Log(_obj.GetComponent<MeshFilter>().mesh);
-            Debug.Log(_obj.GetComponent<MeshRenderer>().material);
+            _obj.GetComponent<MeshRenderer>().enabled = false; // 투명 벽 => Rendering만 하지 않을 뿐, 실제로 Object는 존재함
         }
     }
 }
