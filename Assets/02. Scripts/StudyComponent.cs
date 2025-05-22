a57e4892fefa39061d6e5d55f8d36c20e8f0c339
yangwon-park <ywonp94@gmail.com>
2025-05-22 10:45:53 +0900

    :necktie: GetComponent 활용한 Component 정보 가져오기

--- Assets/02. Scripts/StudyComponent.cs
+++ Assets/02. Scripts/StudyComponent.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace _02._Scripts
 {
@@ -11,8 +12,16 @@ namespace _02._Scripts
         {
             // _obj = GameObject.Find("Main Camera"); // Hierarchy 전체 기준으로 검색
             _obj = GameObject.FindGameObjectWithTag("Player");
-            
             _obj.name = changeName;
+            
+            Debug.Log(_obj.name);
+            Debug.Log(_obj.tag);
+            Debug.Log(_obj.transform.position);
+            Debug.Log(_obj.transform.rotation);
+            Debug.Log(_obj.transform.localScale);
+
+            Debug.Log(_obj.GetComponent<MeshFilter>().mesh);
+            Debug.Log(_obj.GetComponent<MeshRenderer>().material);
         }
     }
 }
