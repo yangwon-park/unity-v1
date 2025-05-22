f982f90f882758f28c7ad4def871b328b879b8f2
yangwon-park <ywonp94@gmail.com>
2025-05-22 11:34:32 +0900

    :necktie: GameObject.SetActive 관련 수업 내용

--- Assets/02. Scripts/StudyComponent.cs
+++ Assets/02. Scripts/StudyComponent.cs
@@ -5,13 +5,18 @@ namespace _02._Scripts
 {
     public class StudyComponent : MonoBehaviour
     {
-        private GameObject _obj; // Unity Editor > Inspector에서 Cube 주입받음
+        private GameObject _gameObject; // Unity Editor > Inspector에서 Cube 주입받음
+        private Transform _transform;
         
         private void Start()
         {
             // _obj = GameObject.Find("Main Camera"); // Hierarchy 전체 기준으로 검색
-            _obj = GameObject.FindGameObjectWithTag("Player");
-            _obj.GetComponent<MeshRenderer>().enabled = false; // 투명 벽 => Rendering만 하지 않을 뿐, 실제로 Object는 존재함
+            _gameObject = GameObject.FindGameObjectWithTag("Player");
+            _transform = GameObject.FindGameObjectWithTag("Player").transform;
+            
+            _gameObject.GetComponent<MeshRenderer>().enabled = false; // 투명 벽 => Rendering만 하지 않을 뿐, 실제로 Object는 존재함
+            
+            _gameObject.SetActive(false);
         }
     }
 }
