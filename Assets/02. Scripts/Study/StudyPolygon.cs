aabd266cab79d7d8d3b9901288d1e30cd2671013
yangwon-park <ywonp94@gmail.com>
2025-05-26 14:39:25 +0900

    :necktie: Polygon 직접 생성 관련 수업 내용

--- Assets/02. Scripts/Study/StudyPolygon.cs
+++ Assets/02. Scripts/Study/StudyPolygon.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
-public class StudyPolygon : MonoBehaviour
+namespace _02._Scripts.Study
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class StudyPolygon : MonoBehaviour
     {
-        
-    }
+        void Start()
+        {
+            var mesh = new Mesh();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            var vertices = new[]
+            {
+                new Vector3(0, 0, 0),
+                new Vector3(1, 0, 0),
+                new Vector3(0, 1, 0),
+                new Vector3(1, 1, 0)
+            };
+
+            var triangles = new[]
+            {
+                0, 2, 1,
+                2, 3, 1
+            };
+
+            var uv = new[]
+            {
+                new Vector2(0, 0),
+                new Vector2(1, 0),
+                new Vector2(0, 1),
+                new Vector2(1, 1)
+            };
+
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
+            mesh.uv = uv;
+
+            var meshFilter = gameObject.AddComponent<MeshFilter>();
+            meshFilter.mesh = mesh;
+            
+            var meshRenderer = gameObject.AddComponent<MeshRenderer>();
+            meshRenderer.material = new Material(Shader.Find("Sprites/Default"));
+            
+            
+        }
     }
 }
