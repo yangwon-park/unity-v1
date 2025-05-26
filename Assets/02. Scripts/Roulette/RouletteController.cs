a7b7e513922d6fc6fd2c6855486ca327035f2d3e
yangwon-park <ywonp94@gmail.com>
2025-05-26 11:50:23 +0900

    :necktie: StudyTransform Script 관련 수업 내용

--- Assets/02. Scripts/Roulette/RouletteController.cs
+++ Assets/02. Scripts/Roulette/RouletteController.cs
@@ -4,10 +4,38 @@ namespace _02._Scripts.Roulette
 {
     public class RouletteController : MonoBehaviour
     {
-        [SerializeField] private float speed = -5f;
+        [SerializeField] private float speed;
+        [SerializeField] private bool isStop;
+        void Start()
+        {
+            speed = 0f;
+        }
+        
         void Update()
         {
             transform.Rotate(Vector3.forward * speed); // z축 기준으로 회전 (파랑)
+            
+            // 마우스 좌클릭 시, 회전 시작
+            if (Input.GetMouseButtonDown(0))
+            {
+                speed = -5f;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isStop = true;
+            }
+
+            if (isStop)
+            {
+                speed *= 0.98f;
+                
+                if (speed > -0.01f)
+                {
+                    speed = 0f;
+                    isStop = false;
+                }
+            }
         }
     }
 }
