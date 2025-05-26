f6b22cf78c1eb13aad6a2c8d9568b3a683113a19
yangwon-park <ywonp94@gmail.com>
2025-05-26 14:12:22 +0900

    :recycle: Study Package 생성

---
+++ Assets/02. Scripts/Study/StudyTransform.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace _02._Scripts
+{
+    public class StudyTransform : MonoBehaviour
+    {
+        [SerializeField] private float moveSpeed = 10f;
+        [SerializeField] private float rotateSpeed = 70f;
+
+        void Update()
+        {
+            var angle = transform.rotation.eulerAngles.y + (rotateSpeed * Time.deltaTime);
+            
+            // 월드 방향으로 이동, 회전
+            // transform.position += Vector3.forward * (moveSpeed * Time.deltaTime);
+            transform.Translate(Vector3.forward * (moveSpeed * Time.deltaTime), Space.World); // 이동
+            // transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime, Space.World);
+
+            // 로컬 방향 이동, 회전
+            // localPosition, localRotation -> 계층 구조가 존재해야함, 부모가 없는 경우 World가 기준이 됨
+            transform.Translate(Vector3.forward * (moveSpeed * Time.deltaTime)); // 이동
+            transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime); // Self 생략
+            
+            // 특정 위치 주변 회전
+            transform.RotateAround(Vector3.zero, Vector3.up, rotateSpeed * Time.deltaTime);
+        }
+    }
+}
