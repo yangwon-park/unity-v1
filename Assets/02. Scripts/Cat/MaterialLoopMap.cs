f975e871372a2e160cb21cc70c5d670d7c71d7ed
yangwon-park <ywonp94@gmail.com>
2025-05-27 17:43:36 +0900

    :recycle: MainTex 변수화

--- Assets/02. Scripts/Cat/MaterialLoopMap.cs
+++ Assets/02. Scripts/Cat/MaterialLoopMap.cs
@@ -4,6 +4,7 @@ namespace _02._Scripts.Cat
 {
     public class MaterialLoopMap : MonoBehaviour
     {
+        private static readonly int MainTex = Shader.PropertyToID("_MainTex");
         private MeshRenderer _meshRenderer;
         [SerializeField] private float speed = 0.1f;
         
@@ -17,9 +18,9 @@ namespace _02._Scripts.Cat
             // 변경된 offset 값
             Vector2 offSet = Vector2.right * (speed * Time.deltaTime);
             
-            // Texture의 Offset을 적용2
+            // Texture의 Offset을 적용
             // _MainTex-> Base Map
-            _meshRenderer.material.SetTextureOffset("_MainTex", _meshRenderer.material.mainTextureOffset + offSet);
+            _meshRenderer.material.SetTextureOffset(MainTex, _meshRenderer.material.mainTextureOffset + offSet);
         }
     }
 }
