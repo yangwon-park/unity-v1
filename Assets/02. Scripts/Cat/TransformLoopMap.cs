34687038fdd21133f8e68201e754c48aeffdf8b1
yangwon-park <ywonp94@gmail.com>
2025-05-27 11:53:52 +0900

    :necktie: 무한 맵 적용 관련 수업 내용

--- Assets/02. Scripts/Cat/TransformLoopMap.cs
+++ Assets/02. Scripts/Cat/TransformLoopMap.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
 
-public class TransformLoopMap : MonoBehaviour
+namespace _02._Scripts.Cat
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class TransformLoopMap : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private float speed;
+        private Vector3 _returnPos;
+        private float _width;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void Start()
+        {
+            var spriteRenderer = GetComponent<SpriteRenderer>();
+            _width = spriteRenderer.bounds.size.x;
+        }
+
+        void Update()
+        {
+            transform.position += Vector3.left * (speed * Time.deltaTime);
+
+            if (transform.position.x <= -_width)
+            {
+                // 현재 위치에서 상대적으로 이동 (연결 유지)
+                transform.position += Vector3.right * (_width * 2);
+            }
+        }
     }
 }
