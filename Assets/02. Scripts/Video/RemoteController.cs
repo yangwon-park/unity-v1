4bc3fb98566086b439d3485c4fe4638eab9d8f54
yangwon-park <ywonp94@gmail.com>
2025-06-11 14:40:41 +0900

    :necktie: TV 리모콘 동작 관련 수업 내용 3차 (Change Channel)

--- Assets/02. Scripts/Video/RemoteController.cs
+++ Assets/02. Scripts/Video/RemoteController.cs
@@ -8,43 +8,57 @@ namespace _02._Scripts.Video
     {
         [SerializeField] private GameObject videoScreen;
         [SerializeField] private Button[] buttonUIs;
+        [SerializeField] private VideoClip[] videoClips;
 
         private bool _isMute;
         private VideoPlayer _videoPlayer;
-        
+        private int _currentClipIndex = 0;
+
         private void Awake()
         {
             _videoPlayer = videoScreen.GetComponent<VideoPlayer>();
+            _videoPlayer.clip = videoClips[0];
         }
-        
+
         private void Start()
         {
             buttonUIs[0].onClick.AddListener(OnPower);
-            buttonUIs[1].onClick.AddListener(Mute);
-            buttonUIs[2].onClick.AddListener(Prev);
-            buttonUIs[3].onClick.AddListener(Next);
+            buttonUIs[1].onClick.AddListener(OnMute);
+            buttonUIs[2].onClick.AddListener(() => OnChangeChannel(true));
+            buttonUIs[3].onClick.AddListener(() => OnChangeChannel(false));
         }
-        
+
         private void OnPower()
         {
             videoScreen.SetActive(!videoScreen.activeSelf);
         }
-        
-        private void Mute()
+
+        private void OnMute()
         {
-            _isMute = !_isMute;
-            _videoPlayer.SetDirectAudioMute(0, !_videoPlayer.GetDirectAudioMute(0));
-            // _videoPlayer.SetDirectAudioMute(0, _videoPlayer.GetDirectAudioMute(0));
+            var currentMuteStatus = !_videoPlayer.GetDirectAudioMute(0);
+            _videoPlayer.SetDirectAudioMute(0, currentMuteStatus);
+
+            // _isMute = !_isMute;
+            // _videoPlayer.SetDirectAudioMute(0, _isMute);
         }
-        
-        private void Prev()
+
+        private void OnChangeChannel(bool isNext)
         {
-            
+            if (isNext)
+            {
+                ChangeChannel(1);
+            }
+            else
+            {
+                ChangeChannel(-1);
+            }
         }
-        
-        private void Next()
+
+        private void ChangeChannel(int direction)
         {
-            
+            _currentClipIndex = (_currentClipIndex + direction + videoClips.Length) % videoClips.Length;
+            _videoPlayer.clip = videoClips[_currentClipIndex];
+            _videoPlayer.Play();
         }
     }
 }
