a7ccde72e96960d0a2382ed1b4708d00eb2ae675
yangwon-park <ywonp94@gmail.com>
2025-06-11 13:46:57 +0900

    :necktie: TV 리모콘 동작 관련 수업 내용 2차 (Mute)

--- Assets/02. Scripts/Video/RemoteController.cs
+++ Assets/02. Scripts/Video/RemoteController.cs
@@ -1,17 +1,49 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 namespace _02._Scripts.Video
 {
     public class RemoteController : MonoBehaviour
     {
         [SerializeField] private GameObject videoScreen;
-        [SerializeField] private Button[] buttonUIs;        
+        [SerializeField] private Button[] buttonUIs;
+
+        private bool _isMute;
+        private VideoPlayer _videoPlayer;
         
-        public void OnPower()
+        private void Awake()
+        {
+            _videoPlayer = videoScreen.GetComponent<VideoPlayer>();
+        }
+        
+        private void Start()
+        {
+            buttonUIs[0].onClick.AddListener(OnPower);
+            buttonUIs[1].onClick.AddListener(Mute);
+            buttonUIs[2].onClick.AddListener(Prev);
+            buttonUIs[3].onClick.AddListener(Next);
+        }
+        
+        private void OnPower()
         {
             videoScreen.SetActive(!videoScreen.activeSelf);
+        }
+        
+        private void Mute()
+        {
+            _isMute = !_isMute;
+            _videoPlayer.SetDirectAudioMute(0, !_videoPlayer.GetDirectAudioMute(0));
+            // _videoPlayer.SetDirectAudioMute(0, _videoPlayer.GetDirectAudioMute(0));
+        }
+        
+        private void Prev()
+        {
             
+        }
+        
+        private void Next()
+        {
             
         }
     }
