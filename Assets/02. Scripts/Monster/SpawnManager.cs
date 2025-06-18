cfc085796a37a71ecdc28d4a89bb104c7b49a048
yangwon-park <ywonp94@gmail.com>
2025-06-18 15:46:39 +0900

    :necktie: 아이템 Drop AddForce 관련 수업 내용

--- Assets/02. Scripts/Monster/SpawnManager.cs
+++ Assets/02. Scripts/Monster/SpawnManager.cs
@@ -14,22 +14,25 @@ namespace _02._Scripts.Monster
             while (true)
             {
                 yield return new WaitForSeconds(3f);
-                
+
                 var randomIndex = Random.Range(0, monsters.Length);
                 var randomX = Random.Range(-8, 9);
                 var randomY = Random.Range(-3, 6);
-                
+
                 var randomPos = new Vector3(randomX, randomY, 0);
-                
+
                 Instantiate(monsters[randomIndex], randomPos, Quaternion.identity);
             }
         }
-        
+
         public void DropCoin(Vector3 dropPos)
         {
-            var randomIndex = Random.Range(0, items.Length);
-            
-            Instantiate(items[randomIndex], dropPos, Quaternion.identity);
+            var item = Instantiate(items[Random.Range(0, items.Length)], dropPos, Quaternion.identity);
+            var rb = item.GetComponent<Rigidbody2D>();
+
+            rb.AddForceY(Random.Range(-2f, 2f), ForceMode2D.Impulse);
+            rb.AddForceY(3f, ForceMode2D.Impulse);
+            rb.AddTorque(Random.Range(-5f, 5f), ForceMode2D.Impulse);
         }
     }
 }
