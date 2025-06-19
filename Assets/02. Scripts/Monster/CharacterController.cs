5e5b8a2ce723b5d5081518043b9cb044ec715a55
yangwon-park <ywonp94@gmail.com>
2025-06-19 13:28:15 +0900

    :necktie: Monster 관련 수업 내용 완료 (Item 드랍 후 Inventory 저장까지)

--- Assets/02. Scripts/Monster/CharacterController.cs
+++ Assets/02. Scripts/Monster/CharacterController.cs
@@ -1,11 +1,12 @@
+using System;
 using System.Collections;
+using _02._Scripts.Monster.Item;
 using UnityEngine;
 
 namespace _02._Scripts.Monster
 {
     public class CharacterController : MonoBehaviour
     {
-        
         [SerializeField] private GameObject hitBox;
         [SerializeField] private float speed = 3f;
         private Animator _animator;
@@ -16,13 +17,13 @@ namespace _02._Scripts.Monster
         {
             _animator = GetComponent<Animator>();
         }
-        
+
         void Update()
         {
             Attack();
             Move();
         }
-        
+
         void Move()
         {
             _h = Input.GetAxis("Horizontal");
@@ -34,8 +35,12 @@ namespace _02._Scripts.Monster
             }
             else
             {
+                var scaleX = _h > 0 ? 1 : -1;
+
+                transform.localScale = new Vector3(scaleX, 1, 1);
+
                 _animator.SetBool("Run", true);
-                
+
                 var dir = new Vector3(_h, _v, 0).normalized;
                 transform.position += dir * (speed * Time.deltaTime);
             }
@@ -53,22 +58,30 @@ namespace _02._Scripts.Monster
         {
             _isAttack = true;
             hitBox.SetActive(true);
-            
+
             yield return new WaitForSeconds(0.25f);
-            
+
             _isAttack = false;
             hitBox.SetActive(false);
         }
-        
+
         void OnTriggerEnter2D(Collider2D other)
         {
             var monster = other.GetComponent<Monster>();
-            
+
             if (monster)
             {
                 // hit 실행
                 StartCoroutine(monster.Hit(1));
             }
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            var item = other.gameObject.GetComponent<IItem>();
+            
+            // hit 실행
+            item?.GetItem();
         }
     }
 }
---
+++ Assets/02. Scripts/Monster/ITalk.cs
@@ -0,0 +1,7 @@
+namespace _02._Scripts.Monster
+{
+    public interface ITalk
+    {
+        void Talk();
+    }
+}
