5a8874acc44c337d5e6d3eabe8276a222692af84
yangwon-park <ywonp94@gmail.com>
2025-06-23 15:53:06 +0900

    :necktie: Joystick 관련 수업 내용 1차

--- Assets/02. Scripts/Knight/JoystickController.cs
+++ Assets/02. Scripts/Knight/JoystickController.cs
@@ -4,6 +4,9 @@ namespace _02._Scripts.Knight
 {
     public class JoystickController : MonoBehaviour
     {
-        
+        public void OnLog(string msg)
+        {
+            Debug.Log(msg);
+        }
     }
 }
