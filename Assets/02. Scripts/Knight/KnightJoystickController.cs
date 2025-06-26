3570c7f904b7a102f61add300295f97de48e1553
yangwon-park <ywonp94@gmail.com>
2025-06-26 09:19:22 +0900

    :necktie: Town용 JoystickController 수정

--- Assets/02. Scripts/Knight/KnightJoystickController.cs
+++ Assets/02. Scripts/Knight/KnightJoystickController.cs
@@ -5,30 +5,16 @@ namespace _02._Scripts.Knight
 {
     public class KnightJoystickController : MonoBehaviour
     {
-        [SerializeField] private float speed = 3f;
-        [SerializeField] private float jumpPower = 10f;
-        [SerializeField] private Button jumpButton;
-        [SerializeField] private Button attackButton;
-        
         private Animator _animator;
         private Rigidbody2D _rigidbody;
         private Vector3 _inputDir;
-        private bool _isGround;
-        private bool _isAttack;
-        private bool _isCombo;
+
+        private float _speed = 3f;
 
         private void Start()
         {
             _animator = GetComponent<Animator>();
             _rigidbody = GetComponent<Rigidbody2D>();
-            
-            jumpButton.onClick.AddListener(Jump);
-            attackButton.onClick.AddListener(Attack);
-        }
-
-        private void Update()
-        {
-            
         }
 
         private void FixedUpdate()
@@ -37,31 +23,17 @@ namespace _02._Scripts.Knight
             HandleMovement();
         }
 
-        private void OnCollisionEnter2D(Collision2D other)
-        {
-            if (!other.gameObject.CompareTag("Ground")) return;
-            _animator.SetBool("isGround", true);
-            _isGround = true;
-        }
-
-        private void OnCollisionExit2D(Collision2D other)
-        {
-            if (!other.gameObject.CompareTag("Ground")) return;
-            _animator.SetBool("isGround", false);
-            _isGround = false;
-        }
-
         public void HandleInput(float x, float y)
         {
             _inputDir = new Vector3(x, y, 0).normalized;
-            
+
             _animator.SetFloat("JoystickX", x);
             _animator.SetFloat("JoystickY", y);
 
             if (_inputDir.x != 0)
             {
                 var scaleX = _inputDir.x > 0 ? 1 : -1;
-                transform.localScale = new Vector3(scaleX, 1, 1);
+                transform.localScale = new Vector3(scaleX, 1f, 1f);
             }
         }
 
@@ -69,44 +41,12 @@ namespace _02._Scripts.Knight
         {
             if (_inputDir.x != 0)
             {
-                _rigidbody.linearVelocityX = _inputDir.x * speed;
+                _rigidbody.linearVelocity = _inputDir * _speed;
+                // _rigidbody.linearVelocityX = _inputDir.x * _speed;
             }
             else
             {
                 _rigidbody.linearVelocityX = 0;
-            }
-        }
-
-        private void Jump()
-        {
-            if (!_isGround) return;
-            _animator.SetTrigger("Jump");
-            _rigidbody.AddForceY(jumpPower, ForceMode2D.Impulse);
-        }
-
-        private void Attack()
-        {
-            if (!_isAttack)
-            {
-                _isAttack = true;
-                _animator.SetTrigger("Attack");
-            }
-            else
-            {
-                _isCombo = true;
-            }
-        }
-
-        private void CheckCombo()
-        {
-            if (_isCombo)
-            {
-                _animator.SetBool("isCombo", true);
-            }
-            else
-            {
-                _animator.SetBool("isCombo", false);
-                _isAttack = false;
             }
         }
     }
--- Assets/02. Scripts/Knight/KnightKeyboardController.cs
+++ Assets/02. Scripts/Knight/KnightKeyboardController.cs
@@ -7,10 +7,15 @@ namespace _02._Scripts.Knight
         private Animator _animator;
         private Rigidbody2D _rigidbody;
         private Vector3 _inputDir;
-        private bool _isJumpInput = false;
-        private bool _isGround = false;
-        [SerializeField] private float speed = 3f;
-        [SerializeField] private float jumpPower = 10f;
+        
+        private bool _isJumpInput;
+        private bool _isGround;
+        private bool _isAttack;
+        private bool _isCombo;
+
+        private float _akDamage = 3f;
+        private float _speed = 3f;
+        private float _jumpPower = 10f;
 
         private void Start()
         {
@@ -22,8 +27,13 @@ namespace _02._Scripts.Knight
         {
             HandleInput();
             HandleSpriteDirection();
-            HandleJump();
             HandleAnimator();
+            Jump();
+
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                Attack();
+            }
         }
 
         private void FixedUpdate()
@@ -45,11 +55,22 @@ namespace _02._Scripts.Knight
             _animator.SetBool("isGround", false);
             _isGround = false;
         }
+        
+        // Collider가 아예 움직임 자체가 없으면 OnOff하여도 동작되지 않음
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Monster"))
+            {
+                Debug.Log($"other :: {other.name}");
+                Debug.Log($"other :: {other.gameObject.name}");
+                Debug.Log($"공격 판정. 피해량 :: {_akDamage}");
+            }
+        }
 
         private void HandleInput()
         {
-            var h = Input.GetAxis("Horizontal");
-            var v = Input.GetAxis("Vertical");
+            var h = Input.GetAxisRaw("Horizontal");
+            var v = Input.GetAxisRaw("Vertical");
             _inputDir = new Vector3(h, v, 0);
 
             if (Input.GetKeyDown(KeyCode.Space) && _isGround)
@@ -62,7 +83,7 @@ namespace _02._Scripts.Knight
         {
             if (_inputDir.x != 0)
             {
-                _rigidbody.linearVelocityX = _inputDir.x * speed;
+                _rigidbody.linearVelocityX = _inputDir.x * _speed;
             }
             else
             {
@@ -70,10 +91,10 @@ namespace _02._Scripts.Knight
             }
         }
 
-        private void HandleJump()
+        private void Jump()
         {
             if (!_isJumpInput || !_isGround) return;
-            _rigidbody.AddForceY(jumpPower, ForceMode2D.Impulse);
+            _rigidbody.AddForceY(_jumpPower, ForceMode2D.Impulse);
             _isJumpInput = false;
         }
 
@@ -86,13 +107,50 @@ namespace _02._Scripts.Knight
 
         private void HandleAnimator()
         {
-            var isMoving = Mathf.Abs(_inputDir.x) > 0.1f;
-            _animator.SetBool("isRun", isMoving);
-
+            _animator.SetFloat("JoystickX", _inputDir.x);
+            _animator.SetFloat("JoystickY", _inputDir.y);
+            
             if (_isJumpInput && _isGround)
             {
                 _animator.SetTrigger("Jump");
             }
+        }
+        
+        private void Attack()
+        {
+            if (!_isAttack)
+            {
+                _isAttack = true;
+                _akDamage = 3f;
+                _animator.SetTrigger("Attack");
+            }
+            else
+            {
+                _isCombo = true;
+            }
+        }
+
+        private void CheckCombo()
+        {
+            if (_isCombo)
+            {
+                _animator.SetBool("isCombo", true);
+                _akDamage = 5f;
+            }
+            else
+            {
+                _animator.SetBool("isCombo", false);
+                _isAttack = false;
+            }
+        }
+
+        // 콤보가 복잡해질 경우를 대비해 EndCombo를 별도의 메소드로 처리 -> Animation Event로 동작하게 만듦
+        // Animator + Script 있는 경우, Script에 있는 메소드에 접근할 수 있음
+        private void EndCombo()
+        {
+            _isAttack = false;
+            _isCombo = false;
+            _animator.SetBool("isCombo", false);
         }
     }
 }
